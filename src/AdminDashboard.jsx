fb0798c358858d63897d273cc1b490635eec91fd
ViktorCiganovic-collab <dcifw09@hotmail.se>
2025-05-26 20:27:45 +0200

    Added admin functions + integrating with database

--- src/AdminDashboard.jsx
+++ src/AdminDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import './styling/AdminDashboard.css';
 import { useTranslation } from 'react-i18next'; 
@@ -6,6 +6,8 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from './components/AuthProvider';
+import axios from "axios";
+
 
 const AdminDashboard = () => {
 
@@ -14,6 +16,17 @@ const AdminDashboard = () => {
     const [show, setShow] = useState(false);
     const { role, setRole, isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
     const navigate = useNavigate();
+    const [addTestTimeForm, setAddTestTimeForm] = useState(false); 
+    const [certId, setCertId] = useState('');
+    const [starttime, setStarttime] = useState('');
+    const [endtime, setEndtime] = useState('');
+    const [error, setError] = useState(false);
+    const [response, setResponse] = useState('');
+    const [addUpdateTestTimeForm, setAddUpdateTestTimeForm] = useState(false);
+    const [testTimeId, setTestTimeId] = useState('');
+    const [deleteFormVisible, setDeleteFormVisible] = useState(false);
+    const [certsDisplayed, setCertsDisplayed] = useState(false);
+    const [certificates, setCertificates] = useState([]);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
@@ -26,10 +39,153 @@ const AdminDashboard = () => {
   }
 
     const toggleBox = (boxName) => {
+          if (addTestTimeForm) {
+    // Om formuläret är öppet, gör inget när man klickar på boxen
+    return;
+  }
     setIsOpen(prev => (prev === boxName ? null : boxName));
   };
 
+    const Openformfornewtesttimes = () => {
+    setAddTestTimeForm(!addTestTimeForm);
+    }
 
+    const openformforupdatingtesttimes = () => {
+    setAddUpdateTestTimeForm(!addUpdateTestTimeForm);
+    setAddTestTimeForm(false);
+    }
+
+    const Addnewtesttime = async (event) => {
+    event.preventDefault();
+
+    if (!certId || !starttime || !endtime) {
+      setError('Vänligen fyll i alla fält!');
+      return;
+    }
+
+   const testTime = {
+    certId: Number(certId),  // Säkerställ att det är ett nummer
+    examStartingTime: starttime,
+    examEndingTime: endtime
+  };
+
+    try {
+        const res = await axios.post('http://3.90.225.16:5011/api/ExamDate', testTime, {
+        headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      }
+        });
+        setResponse(res.data);
+        setCertId('');
+        setStarttime('');
+        setEndtime('');
+        setError(null);
+    } 
+
+    catch (error) {
+      setError(`Något gick fel: ${error.message || "Vänligen försök igen senare."}`);      
+    }
+
+    }
+
+    const UpdateTesttime = async (event) => {
+        event.preventDefault();
+        
+        if (!testTimeId || !starttime || !endtime) {
+        setError('Vänligen fyll i alla fält!');
+        return;
+        }
+
+        if (new Date(endtime) <= new Date(starttime)) {
+            setError('Slutdatum måste vara efter startdatum.');
+            return;
+        }
+
+        const updatedTestTime = {
+            id: Number(testTimeId),  // testtidens id
+            examStartingTime: starttime,
+            examEndingTime: endtime,
+        };
+
+        try {
+            const res = await axios.put(`http://3.90.225.16:5011/api/ExamDate/${updatedTestTime.id}`, updatedTestTime, {
+            headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            }  
+            });
+        setResponse('Testtiden har uppdaterats!');
+        setError(null);
+        setTestTimeId('');
+        setCertId('');
+        setStarttime('');
+        setEndtime('');
+        }
+
+        catch (error) {
+        setError(`Något gick fel: ${error.message || "Vänligen försök igen senare."}`);
+  }
+
+    }
+
+    const openformDeleteTesttime = async () => {
+        setDeleteFormVisible(!deleteFormVisible);
+        setAddUpdateTestTimeForm(false);
+        setAddTestTimeForm(false);
+
+    }
+
+    const DeleteTesttime = async (event) => {
+         event.preventDefault();
+
+        if (!testTimeId || !starttime || !endtime) {
+        setError('Vänligen fyll i alla fält!');
+        return;        
+        }
+
+        if (new Date(endtime) <= new Date(starttime)) {
+        setError('Slutdatum måste vara efter startdatum.');
+        return;
+        }
+
+            const deletedTesttime = {
+            id: Number(testTimeId),  // testtidens id
+            examStartingTime: starttime,
+            examEndingTime: endtime,
+        };
+
+        try {
+        
+        const res = await axios.delete(`http://3.90.225.16:5011/api/ExamDate/${testTimeId}`)
+        
+        setResponse('Testtiden har raderats!');
+        setError(null);
+
+        }   
+
+        catch (error) {
+        setError(`Något gick fel: ${error.message || "Vänligen försök igen senare."}`);
+  }
+    }
+
+    const viewTesttimes = async () => {
+  const newState = !certsDisplayed;
+  setCertsDisplayed(newState);
+
+  if (newState) {
+    try {
+      const result = await axios.get('http://3.90.225.16:5011/api/ExamDate');
+      setCertificates(result.data);
+      console.log(result.data);
+      setError(null);
+    } catch (error) {
+      setError(`Något gick fel: ${error.message || "Vänligen försök igen senare."}`);
+    }
+  }
+};
+
+    
 
   return (
     <div className='adminDashboard'>
@@ -70,20 +226,99 @@ const AdminDashboard = () => {
 
                     {isOpen === 'box2' && (
                     <div className={`box-content open`}>
-                        <button className="btn btn-primary mx-5 my-2">Redigera certifikat</button>
+                        <button className="btn btn-primary">Redigera certifikat</button>
                         <button className="btn btn-primary">Lägg till certifikat</button>
+                        <button className='btn btn-primary'>Se alla certifikat</button>
                     </div>
                 )}
             </div>  
 
-            <div className='box box3' onClick={() => toggleBox('box3')}>
+            <div className='box box3 p-2' onClick={() => toggleBox('box3')}>
                 <h1>{t('edit_test_times')}</h1>
 
                     {isOpen === 'box3' && (
                     <div className={`box-content open`}>
-                        <button className="btn btn-primary">Upddatera testtider</button>
+                        <button className='btn btn-primary'  onClick={(e) => {e.stopPropagation(); viewTesttimes();}}>Se alla testtillfällen</button>
+                        <button className="btn btn-primary" onClick={openformforupdatingtesttimes}>Uppdatera testtider</button>
+                        <button className="btn btn-primary" onClick={Openformfornewtesttimes}>Lägg till testtider</button>
+                        <button className='btn btn-primary' onClick={openformDeleteTesttime}>Radera en testtid</button>
                     </div>
                 )}
+                {certsDisplayed && (<div onClick={e => e.stopPropagation()} className='d-flex justify-content-center flex-column align-items-center'>
+
+                    <button className="btn btn-primary" onClick={() => setCertsDisplayed(!certsDisplayed)}>Go Back</button>
+                    <h2 className='text-center my-2'>Alla inlagda testtider</h2>
+
+                      {certificates.map((certificate, index) => (
+                        <div className='text-center'>
+                        <p key={index}>{certificate.certName}</p>
+                        <p>Test id: {certificate.id}</p>
+                        <p>Testtid: {certificate.examStartingTime} - {certificate.examEndingTime}</p>
+                        <p>Pris: {certificate.price} SEK</p>
+                        <p></p>
+                        <div className='grey_line'></div>
+                        </div>
+                    ))}
+
+                </div>)}
+                {/* Form for adding test times */}
+                {addTestTimeForm && (
+                    <div className='d-flex justify-content-center flex-column align-items-center'>
+                    <h3 className='text-center'>Lägg till testtillfälle</h3>
+                    <button className="btn btn-primary" onClick={() => setAddTestTimeForm(!addTestTimeForm)}>Go Back</button>
+                    <form onSubmit={Addnewtesttime} className='d-flex flex-column justify-content-center align-items-center'>
+
+                        <label htmlFor="name">Cert Id:</label>
+                        <input className='my-2' value={certId} onChange={(event) => setCertId(event.target.value)} type="number"></input>
+                        
+                        <label htmlFor="name">Startdatum för test:</label>
+                        <input className='my-2' value={starttime} onChange={(event) => setStarttime(event.target.value)} type="datetime-local"></input>
+                        
+                        <label htmlFor="name">Slutdatum för test:</label>
+                        <input className='my-2' min={starttime} value={endtime} onChange={(event) => setEndtime(event.target.value)} type="datetime-local"></input>
+                        <button type="submit" className='btn btn-primary mt-3 p-3'>Lägg till</button>
+                    </form>
+                    {response && <p>Testtiden har lagts till</p>}
+                    {error && <p className="error-message">{error}</p>}
+                    </div>
+                )}
+                {/* Form for adding test times */}
+                {addUpdateTestTimeForm && (
+                                        <div onClick={e => e.stopPropagation()} className='d-flex justify-content-center flex-column align-items-center'>
+                    <h3 className='text-center'>Uppdatera ett testtillfälle</h3>
+                    <button className="btn btn-primary" onClick={() => setAddUpdateTestTimeForm(!addUpdateTestTimeForm)}>Go Back</button>
+                    <form onSubmit={UpdateTesttime} className='d-flex flex-column justify-content-center align-items-center'>
+
+                        <label>Testtidens ID:</label>
+                        <input className='my-2' value={testTimeId} onChange={(e) => setTestTimeId(e.target.value)} type="number" />
+                        
+                        <label htmlFor="name">Nytt startdatum för test:</label>
+                        <input className='my-2' value={starttime} onChange={(event) => setStarttime(event.target.value)} type="datetime-local"></input>
+                        
+                        <label htmlFor="name">Nytt slutdatum för test:</label>
+                        <input className='my-2' min={starttime} value={endtime} onChange={(event) => setEndtime(event.target.value)} type="datetime-local"></input>
+                        <button type="submit" className='btn btn-primary mt-3 p-3'>Lägg till</button>
+                    </form>
+                    {response && <p>Testtiden har lagts till</p>}
+                    {error && <p className="error-message">{error}</p>}
+                    </div>
+                )}
+                {deleteFormVisible && (<div onClick={e => e.stopPropagation()} className='d-flex justify-content-center flex-column align-items-center'>
+                    <button className="btn btn-primary" onClick={() => setDeleteFormVisible(false)}>Go Back</button>
+                    <form className='d-flex flex-column justify-content-center align-items-center' onSubmit={DeleteTesttime}>
+                        <label>Testtidens ID:</label>
+                        <input className='my-2' value={testTimeId} onChange={(e) => setTestTimeId(e.target.value)} type="number" />
+
+                        <label htmlFor="name">startdatum för test:</label>
+                        <input className='my-2' value={starttime} onChange={(event) => setStarttime(event.target.value)} type="datetime-local"></input>
+
+                        <label htmlFor="name">Slutdatum för test:</label>
+                        <input className='my-2' min={starttime} value={endtime} onChange={(event) => setEndtime(event.target.value)} type="datetime-local"></input>
+                        <button type="submit" className='btn btn-primary mt-3 p-3'>Radera testtid</button>
+                    </form>
+                    {response && <p>Testtiden har raderats</p>}
+                    {error && <p className="error-message">{error}</p>}
+                </div>)}
             </div>  
 
             <div className='box box4' onClick={() => toggleBox('box4')}>
